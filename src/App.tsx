5c030bd6261be843cc8bc0ea6208737b00102b3e
Chavidu <chavidukalpana40@gmail.com>
2023-09-11 23:38:18 +0530

    feat: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
 import './resources/styles.css'
 
+
+
+import Header from './components/header_footer/Header'
+
 const App = ()=> {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      Hello
+      <Header/>
     </div>
   )
 }
