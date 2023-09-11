c399fd575926ab9ec2f0926c404a47d124820cfd
Chavidu <chavidukalpana40@gmail.com>
2023-09-11 22:01:01 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './resources/styles.css'
+
+const App = ()=> {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Hello
+    </div>
+  )
+}
+
+export default App
